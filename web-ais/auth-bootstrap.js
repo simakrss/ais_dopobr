5ea22f398418e87db9740bf3a80874daf09e0c42
TheDiVaZo (home pc) <thedivazo.dev@mail.ru>
2026-08-31 06:38:23 +0300

    Compact student communication cards

--- web-ais/auth-bootstrap.js
+++ web-ais/auth-bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const AUTH_BUILD = "20260831-dop-certificate-template-v1";
+  const AUTH_BUILD = "20260831-compact-communications-v1";
   const DISMISSIBLE_MODAL_BACKDROP_SELECTOR = ".modal-backdrop, .partner-modal-backdrop, [data-documents-backdrop]";
   const baseUrl = new URL(".", document.currentScript?.src || window.location.href);
   const app = document.getElementById("app");
